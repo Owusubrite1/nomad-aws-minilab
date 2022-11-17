01f1f1b24368b6897efafcb9de162ee697734fb5
owusubrite <owusubrite1@gmail.com>
2022-11-17 00:11:00 -0700

    first1

--- vars.tf
+++ vars.tf
@@ -43,22 +43,16 @@ variable "nomad_node_ami_id" {
 #}
 
 
-variable "nomad_node_count" {
-  description = "The number of server nodes (should be 3 or 5)"
-  type        = number
-  default     = 3
-}
-
 variable "nomad_client_count" {
   description = "The number of server nodes (should be 3 or 5)"
-  type        = number
-  default     = 3
+  #type        = number
+  #default     = 3
 }
 
 variable "nomad_server_count" {
   description = "The number of server nodes (should be 3 or 5)"
-  type        = number
-  default     = 3
+  #type        = number
+  #default     = 3
 }
 
 
