ff58f27f54946d307a23e3de7fd75fafedb3cba7
owusubrite <owusubrite1@gmail.com>
2022-11-16 23:56:10 -0700

    first1

--- vars.tf
+++ vars.tf
@@ -34,7 +34,7 @@ variable "nomad_node_ami_id" {
 	 type        = string
   #default     = "ami-064ff912f78e3e561" # Amazon Linux
   #default     = "ami-00978328f54e31526" # Ubuntu Linux
-   default      = "ami-09d3b3274b6c5d4aa" #By Bright
+   default      = "ami-0b0dcb5067f052a63" #By Bright
 }
 
 #variable "nomad_node_count" {
